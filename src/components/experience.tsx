6a2c17c61e4bbb6f04a74d952f78fb1a32289bfe
Christian Jöcker <c.jocker@hotmail.com>
2025-04-24 09:45:06 +0200

    update design

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -18,7 +18,7 @@ export const Experience = ({ experience }: ExperienceProps) => {
 				{experience.positions.map((position) => {
 					return (
 						<div className="mb-5 last:mb-0" key={position.title}>
-							<span className=" font-bold text-lg/1">{position.title}</span>
+							<span className=" font-bold text-lg/1 text-white">{position.title}</span>
 							<span className="ml-2 text-(--subtitle-color)">
 								{formatTimePeriod(position.startDate, position.endDate)}
 							</span>
--- src/utils/export-pdf.ts
+++ src/utils/export-pdf.ts
@@ -5,7 +5,7 @@ import puppeteer from "puppeteer";
 	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
 
-	await page.goto("http://localhost:3000/", { waitUntil: "networkidle2" });
+	await page.goto("http://localhost:3000/?lang=en", { waitUntil: "networkidle2" });
 
 	await page.pdf({
 		path: "export.pdf",
